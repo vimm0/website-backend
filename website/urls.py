1038439936d6d11adf7cad0b75bacc06604d27aa
vimm0 <vimmrana0@gmail.com>
2018-12-01 18:46:05 +0545

    add event [Proof of Concept]

--- website/urls.py
+++ website/urls.py
@@ -19,12 +19,20 @@ from django.conf.urls import url, include
 from django.contrib.auth.models import User
 from rest_framework import serializers, viewsets, routers
 
-
 # Serializers define the API representation.
+from apps.event.models import Event
+
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'is_staff')
+
+
+class EventSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Event
+        fields = '__all__'
 
 
 # ViewSets define the view behavior.
@@ -33,9 +41,15 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
 # Routers provide a way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'event', EventViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
